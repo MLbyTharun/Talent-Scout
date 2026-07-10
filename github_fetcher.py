683c58ea93f3cf0e66bfa350e1915a48788c44b9
tharun <tarun.pc069@gmail.com>
2026-07-10 22:56:20 +0530

    added function to return list of repos of the candidate

--- github_fetcher.py
+++ github_fetcher.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -7,3 +8,29 @@ HEADERS = {
     "Authorization": f"Bearer {GITHUB_TOKEN}" if GITHUB_TOKEN else "",
     "Accept": "application/vnd.github+json",
 }
+
+def get_user_repo(username :str, include_forks : bool = False)->list[dict]:
+    repos = []
+    page = 1
+
+    while True:
+        resp = requests.get(
+            f"https://api/github.com/users/{username}/repos",
+            headers = HEADERS,
+            params = {"per_page" : 100,"page":1, "sort":"pushed","direction":"desc",}
+            )
+        resp.raise_for_status() # err check
+        batch = resp.json()
+
+        if not batch:  # breaks if no outptu
+            break
+        repos.extend(batch)
+        page += 1
+
+    if not include_forks: # else alll
+        repos =  [r for r in repos if not r["fork"]]
+        
+    return repos
+    
+r = get_user_repo("MLbyTharun")
+print(r)

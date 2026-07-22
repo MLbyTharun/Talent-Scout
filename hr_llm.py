63415a966ee011e0544104093a2a3bcf5774d627
tharun <tarun.pc069@gmail.com>
2026-07-22 22:27:38 +0530

    added summarisation before judging

--- hr_llm.py
+++ hr_llm.py
@@ -4,7 +4,7 @@ import os
 from langchain.chat_models import init_chat_model
 
 from langchain_core.messages import SystemMessage, HumanMessage
-from llm_config import SYSTEM_PROMPT,RESUME,JD
+from llm_config_bc import SYSTEM_PROMPT,RESUME,JD, SYSTEM_PROMPT_SUM
 
 from extracter import extract_github_links
 from github_fetcher import get_evaluable_profiles
@@ -20,3 +20,37 @@ groq = init_chat_model(
     temperature=0,
     api_key = os.getenv("GOOGLE_API_KEY")
 )
+
+# Summarisation Block
+
+readme = groq.invoke([
+    SystemMessage(content = SYSTEM_PROMPT_SUM),
+    HumanMessage(content = f"""<JOB_DESCRIPTION>
+                    {JD}
+                    </JOB_DESCRIPTION>
+
+                    <README_FILES>
+                    {data}
+                    </README_FILES>)""")
+])
+
+# Final Block
+
+response = groq.invoke([
+    SystemMessage(content = SYSTEM_PROMPT),
+    HumanMessage(content = f"""<JOB_DESCRIPTION>
+                    {JD}
+                    </JOB_DESCRIPTION>
+
+                    <RESUME>
+                    {RESUME}
+                    </RESUME>
+
+                    <README_FILES>
+                    {readme}
+                    </README_FILES>)""")
+])
+
+print("START")
+print(response.text)
+print("END")
